110c6267305a0ac85816dc52a61cbcba90c8d884
arioum <amoghrao121@gmail.com>
2024-05-08 13:14:55 +0530

    client init - (viteTS, tailwind, shadcn, react router)

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import Dialogue from './components/Dialogue';
+
+const App = () => {
+  return (
+    <div>
+      <img src="logo.svg" alt="logo" />
+      <Dialogue />
+    </div>
+  );
+};
+
+export default App;
